c05af0fb3b709f345385f275f9dc39811024edbf
suvobd100 <makbarbd@gmail.com>
2025-01-22 16:46:14 +0600

    added bsic structure of vit,telwind,disy and router

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <p className="text-6xl bg-blue-400">
+        Click on the Vite and React logos to learn more
+        <br />
+        <button className="btn btn-warning">
+          Clicked for Warning
+        </button>
+      </p>
+    </>
+  )
+}
+
+export default App
